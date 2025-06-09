318d9980c9e57fdf9d1729c8aac1782644f6dec9
Manuel Rueda <manolorueda@gmail.com>
2025-06-08 23:26:31 -0700

    Add posted_at column to tweet_analysis table for tracking analysis posting time

--- sql/create_tweet_analysis.sql
+++ sql/create_tweet_analysis.sql
@@ -5,5 +5,6 @@ CREATE TABLE IF NOT EXISTS tweet_analysis (
     end_date TIMESTAMP NOT NULL,
     unique_tweets INTEGER NOT NULL,
     thinking_process TEXT,
-    response TEXT
+    response TEXT,
+    posted_at TIMESTAMP WITH TIME ZONE DEFAULT NULL  -- When analysis was posted (mirrors reddit_analysis.reported_at)
 );
